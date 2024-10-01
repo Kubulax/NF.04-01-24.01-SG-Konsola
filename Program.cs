0e6dd3e1da9493c5687455cedba7f4454baff138
jakub.koza <jakub.koza@S50-12>
2024-10-01 10:29:18 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Konsolowa2
+{
+    internal class Program
+    {
+        static string CheckGender(string pesel)
+        {
+
+        }
+
+        static string ValidatePesel(string pesel) 
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
